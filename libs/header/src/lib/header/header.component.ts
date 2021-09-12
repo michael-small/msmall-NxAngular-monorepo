5dfff84f41c28e62df47d6061fcb118e803cfef1
Michael <small203@morris.umn.edu>
2021-09-12 17:31:39 -0500

    Add storybook, create Header comp w/ StB & e2e
    Tutorial followed "Modern Angular with Nx Dev Tools" from official Nrwl channel
    
    https://www.youtube.com/watch?v=cXOkmOy-8dk&t=425s

---
+++ libs/header/src/lib/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'msmall-angular-monorepo-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
